992d200b368ad8a348c0ecebc96b34129410bdc9
Adriano Shineyder <adrianoshineyder@hotmail.com>
2021-02-08 20:05:52 -0300

    Adicionado os Mapas e controles de exibição

--- includes/header.php
+++ includes/header.php
@@ -8,6 +8,7 @@
 		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 		<!--Import materialize.css-->
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 		<!--Let browser know website is optimized for mobile-->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
