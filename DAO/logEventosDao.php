01634897293067721c35e0b1b373c6ec31b02085
shineyder <78420043+shineyder@users.noreply.github.com>
2021-02-12 17:13:03 -0300

    Pequenas Correções

--- DAO/logEventosDao.php
+++ DAO/logEventosDao.php
@@ -6,21 +6,17 @@ class EventoDAO
 {
     public function create()
     {
-        
     }
 
     public function read()
     {
-        
     }
 
     public function update()
     {
-        
     }
 
     public function delete()
     {
-        
     }
 }
--- DAO/relatorioDao.php
+++ DAO/relatorioDao.php
@@ -6,21 +6,17 @@ class RelatorioDAO
 {
     public function create()
     {
-        
     }
 
     public function read()
     {
-        
     }
 
     public function update()
     {
-        
     }
 
     public function delete()
     {
-        
     }
 }
